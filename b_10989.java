6cadebb824aeff718444b00667acabdbff0e1e91
SONGHEE BAEK <baeksh0330@naver.com>
2021-07-19 16:51:28 +0900

    107898

---
+++ b_10989.java
@@ -0,0 +1,25 @@
+package AlgorithmStudy;
+import java.util.*;
+public class b_10989 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		int []arr= new int[T];
+		for(int i=0; i<T; i++)
+		{
+			int n=sc.nextInt();
+			arr[i]=n;
+		}
+		Arrays.sort(arr);
+		
+		for(int i=0; i<T; i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+
+}
